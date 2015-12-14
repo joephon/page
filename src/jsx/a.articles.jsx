619e646051a2211ae0b6be5c6c122755949460d4
joephon <306519779@qq.com>
2015-12-15 03:09:45 +0800

    almost finished articles section

--- src/jsx/a.articles.jsx
+++ src/jsx/a.articles.jsx
@@ -3,6 +3,8 @@
 import React from 'react';
 require('../css/a.articles.sass');
 
+let data = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+
 class Articles extends React.Component {
   constructor(props) {
     super(props);
@@ -37,6 +39,7 @@ class Articles extends React.Component {
                 <div className='button-down'
                      onClick={this.selected.bind(this)}>All</div>
               </div>
+              <h1>Articles</h1>
               <div className='button new'
                    style={{
                      color: this.state.selected === 'New'
@@ -52,6 +55,48 @@ class Articles extends React.Component {
                 <div className='button-down'
                      onClick={this.selected.bind(this)}>New</div>
               </div>
+            </div>
+            <div className='content'>
+              <div className='header'>
+                <div className='item select-all'>
+                  <input type='checkbox' name='select-all' className='checkbox'/>
+                </div>
+                <div className='item number'>#</div>
+                <div className='item title'>Title</div>
+                <div className='item views'>Views</div>
+                <div className='item comments'>Comments</div>
+                <div className='item date'>Date</div>
+                <div className='item edit'>Edit</div>
+                <div className='item delete'>Delete</div>
+              </div>
+              {data.map((item) => {
+                return (
+                    <div key={item} className='body'>
+                      <div className='item select-all'>
+                        <input type='checkbox' name='select-one' className='checkbox'/>
+                      </div>
+                      <div className='item number'>{item}</div>
+                      <div className='item title'>Title</div>
+                      <div className='item views'>
+                        <span className='glyphicon glyphicon-eye-open'></span>
+                        <span>200</span>
+                      </div>
+                      <div className='item comments'>
+                        <span className='glyphicon glyphicon-comment'></span>
+                        <span>50</span>
+                      </div>
+                      <div className='item date'>
+                        <span>2015年12月14日</span>
+                      </div>
+                      <div className='item edit'>
+                        <span className='glyphicon glyphicon-edit'></span>
+                      </div>
+                      <div className='item delete'>
+                        <span className='glyphicon glyphicon-trash'></span>
+                      </div>
+                    </div>
+                  )
+              })}
             </div>
           </div>
         </div>
